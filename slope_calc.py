fe58c3a4dc8d17f946f44e989f2a2019367bf2f7
kalkulator413 <kalkin413@gmail.com>
2024-01-29 07:31:43 -0800

    combine slope calcs with eof calcs

--- slope_calc.py
+++ slope_calc.py
@@ -8,25 +8,29 @@ from argo_nn.Grabber import *
 
 # TODO: scale coviariance matrices by Rossby deformation radius
 
-def get_opt_params(subsample_depth, rossby, name):
+def get_opt_params(subsample_depth, rossby, name, plot=True):
 
 	rossby_xdist, rossby_ydist = rossby.return_rossby_def(geopy.Point(np.mean(subsample_depth.latlist),np.mean(subsample_depth.lonlist)))	
 	x,y = np.meshgrid(subsample_depth.lonlist-np.mean(subsample_depth.lonlist),subsample_depth.latlist-np.mean(subsample_depth.latlist))
 	z = subsample_depth.grid[len(subsample_depth.latlist) // 2][len(subsample_depth.lonlist) // 2]
+
 	H = np.array(list(zip(x.ravel(),y.ravel())))		
 	R_inv = np.diag(np.exp(-((x.ravel()/rossby_xdist)**2+(y.ravel()/rossby_ydist)**2))) # R sets the observational noise
-	P_inv = np.diag([1/5])**2 # ratio of P to R is noise to signal
-	denom = np.linalg.inv(H.T.dot(R_inv.dot(H))+P_inv)
-	gain = denom.dot(H.T.dot(R_inv))
-	a = gain.dot(subsample_depth.grid.ravel()-z)
+	P_inv = np.diag(np.array([1, 1]))**2 # ratio of P to R is noise to signal
+	denom = np.linalg.inv(H.T @ R_inv @ H + P_inv)
+	gain = R_inv @ H @ denom
+	a = (subsample_depth.grid.ravel()-z) @ gain
 
-	fig, ax = plt.subplots(1, 1)
-	ax.imshow(subsample_depth.grid, cmap=plt.cm.jet, origin='lower',extent=(x.min(), x.max(), y.min(), y.max()))
-	ax.plot([0,a[0]/max(a)], [0,a[1]]/max(a))
-	plt.show()
+	if plot:
+		fig, ax = plt.subplots(1, 1)
+		ax.imshow(subsample_depth.grid, cmap=plt.cm.jet, origin='lower',extent=(x.min(), x.max(), y.min(), y.max()))
+		ax.plot([0,a[0]/max(np.abs(a))], [0,a[1]/max(np.abs(a))])
+		plt.show()
 
-	# for when i'm using the remote server, because it doesnt show the plots normally
-	# plt.savefig(name)
+		# for when i'm using the remote server, because it doesnt show the plots normally
+		# plt.savefig(name)
+
+	return a[0]/max(np.abs(a)), a[1]/max(np.abs(a))
 
 if __name__ == '__main__':
 	world_depth = AvisoGrabber(2017, 11, 8)
